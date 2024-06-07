94c2e08dd6797a88808ed74a233dbc0d569f1179
huunam123 <huunam123@example.com>
2024-06-07 17:10:02 +0700

    fixx  7/6/2024

--- src/Views/Default/Components/Banner.js
+++ src/Views/Default/Components/Banner.js
@@ -2,66 +2,16 @@
 
 /* Package System */
 import React from "react";
-import Link from "next/link";
-
-/* Application */
-import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from "swiper";
-import { Swiper, SwiperSlide } from "swiper/react";
-import Button from "@mui/material/Button";
-import { fetchApi, changeToSlug } from "@helpers/Common";
-
-/* Package style */
-//import 'swiper/swiper-bundle.min.css';
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
-import { isIOS } from "react-device-detect";
-import Countdown from "react-countdown";
 class Banner extends React.Component {
   constructor(props) {
     super(props);
     this._isMounted = false;
-    this.state = {
-      banners: [],
-      hideDescription: false,
-    };
+    this.state = {};
   }
-
-  getHomeBanner = () => {
-    let _result = [];
-    _result.push({
-      type: "cover",
-      id: "",
-      // 'title': "Bạn muốn trở thành nhân vật xuất hiện trên MCV Network?",
-      thumbnail: "/images/banner1.png",
-      link: "",
-    });
-    return _result;
-  };
 
   componentDidMount() {
     this._isMounted = true;
-    Promise.all([this.getHomeBanner()])
-      .then((resp) => {
-        let _banners = resp[0].concat(
-          typeof resp[1] !== "undefined" ? resp[1] : []
-        );
-        this.setState({ banners: _banners });
-      })
-      .catch((e) => console.log(e));
   }
-
-  hanldeDownload = () => {
-    let _linkDL = isIOS == true ? "" : "";
-    window.location = _linkDL;
-  };
-
-  handleLink = (e) => {
-    console.log("handleLink", e);
-    let _link = e.currentTarget.dataset.link;
-    if (_link && _link != "null") window.location = _link;
-  };
 
   componentWillUnmount() {
     this._isMounted = false;
@@ -70,7 +20,7 @@ class Banner extends React.Component {
   render() {
     return (
       <React.Fragment>
-         <section className="banner"> 
+        <section className="banner">
           <div className="container">
             <div className="banner-content">
               <div className="banner-">
@@ -385,7 +335,7 @@ class Banner extends React.Component {
               </div>
             </div>
           </div>
-         </section> 
+        </section>
       </React.Fragment>
     );
   }
--- src/Views/Default/Components/Header.js
+++ src/Views/Default/Components/Header.js
@@ -1,108 +1,58 @@
 "use strict";
 
-/* Package System */
 import React from "react";
-import Link from "next/link";
-import { withRouter } from "next/router";
-import { connect } from "react-redux";
-
-/* Application */
 import Action from "@libs/Action";
-import ClickAwayListener from "@mui/material/ClickAwayListener";
 import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import HideOnScroll from "@views/Default/Components/HideOnScroll";
-import Image from "next/image";
-
-/* Package style */
 import CssBaseline from "@mui/material/CssBaseline";
 
 class Header extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      bgTransparent: true,
-    };
-    this.handleScroll = this.handleScroll.bind(this);
+    this.state = {};
   }
-
-  componentDidMount() {
-    window.addEventListener("scroll", this.handleScroll);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener("scroll", this.handleScroll);
-  }
-
-  handleScroll = () => {
-    if (window.pageYOffset > 0) {
-      this.setState({ bgTransparent: false });
-    } else {
-      this.setState({ bgTransparent: true });
-    }
-  };
-
-  handleClickOutSide = () => {
-    let _status = this.props.stateStatus.open;
-    if (_status.notification == true ) {
-      this.props.resetOpen();
-    }
-  };
 
   render() {
     return (
       <React.Fragment>
         <CssBaseline />
-        <HideOnScroll {...this.props}>
-          <AppBar
-            id="nl-header"
-            // color="inherit"
-            // className={this.state.bgTransparent == true ? "transparent" : ""}
-            // position="static"
-          >
-            <Toolbar>
-              <div
-                id="nl-headerr__mastehead"
-                // className={
-                //   this.state.bgTransparent == true ? "transparent" : ""
-                // }
-              >
-                <div className="d-flex-">
-                  <div className="menu">
-                    <div className="doctor-text">
-                      <div className="group-doctor">
-                        <svg
-                          width="29"
-                          height="43"
-                          viewBox="0 0 29 43"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <g clip-path="url(#clip0_739_258)">
-                            <path
-                              d="M24.7585 12.4611C23.5615 11.2732 22.1929 10.3189 20.7066 9.62C21.4986 11.291 22.2667 12.9996 22.9331 14.627C27.3082 19.1588 27.2523 26.3773 22.7595 30.8359L14.5 39.0324L6.2425 30.8359C4.03598 28.6462 2.821 25.7358 2.821 22.6414C2.821 19.5469 3.97413 16.7949 6.06694 14.627C6.73328 12.9996 7.50138 11.291 8.29341 9.62C6.8071 10.3189 5.4385 11.2732 4.24147 12.4611C-1.41449 18.0739 -1.41449 27.2069 4.24147 32.8197L14.5 43L24.7585 32.8197C30.4145 27.2069 30.4145 18.0739 24.7585 12.4611Z"
-                              fill="#0188F1"
-                            />
-                            <path
-                              d="M14.5 0C12.489 3.67061 5.04148 17.5849 5.04148 22.1821C5.04148 27.3573 9.28495 31.5684 14.5 31.5684C19.7151 31.5684 23.9585 27.3573 23.9585 22.1821C23.9585 17.5849 16.511 3.67061 14.5 0ZM20.7545 23.3383H16.5848V27.4761H12.4152V23.3383H8.24553V19.2004H12.4152V15.0626H16.5848V19.2004H20.7545V23.3383Z"
-                              fill="#CD1C30"
-                            />
-                          </g>
-                          <defs>
-                            <clipPath id="clip0_739_258">
-                              <rect width="29" height="43" fill="white" />
-                            </clipPath>
-                          </defs>
-                        </svg>
+        <AppBar id="nl-header">
+          <div id="nl-headerr__mastehead">
+            <div className="d-flex-">
+              <div className="menu">
+                <div className="doctor-text">
+                  <div className="group-doctor">
+                    <svg
+                      width="29"
+                      height="43"
+                      viewBox="0 0 29 43"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <g clip-path="url(#clip0_739_258)">
+                        <path
+                          d="M24.7585 12.4611C23.5615 11.2732 22.1929 10.3189 20.7066 9.62C21.4986 11.291 22.2667 12.9996 22.9331 14.627C27.3082 19.1588 27.2523 26.3773 22.7595 30.8359L14.5 39.0324L6.2425 30.8359C4.03598 28.6462 2.821 25.7358 2.821 22.6414C2.821 19.5469 3.97413 16.7949 6.06694 14.627C6.73328 12.9996 7.50138 11.291 8.29341 9.62C6.8071 10.3189 5.4385 11.2732 4.24147 12.4611C-1.41449 18.0739 -1.41449 27.2069 4.24147 32.8197L14.5 43L24.7585 32.8197C30.4145 27.2069 30.4145 18.0739 24.7585 12.4611Z"
+                          fill="#0188F1"
+                        />
+                        <path
+                          d="M14.5 0C12.489 3.67061 5.04148 17.5849 5.04148 22.1821C5.04148 27.3573 9.28495 31.5684 14.5 31.5684C19.7151 31.5684 23.9585 27.3573 23.9585 22.1821C23.9585 17.5849 16.511 3.67061 14.5 0ZM20.7545 23.3383H16.5848V27.4761H12.4152V23.3383H8.24553V19.2004H12.4152V15.0626H16.5848V19.2004H20.7545V23.3383Z"
+                          fill="#CD1C30"
+                        />
+                      </g>
+                      <defs>
+                        <clipPath id="clip0_739_258">
+                          <rect width="29" height="43" fill="white" />
+                        </clipPath>
+                      </defs>
+                    </svg>
 
-                        <div className="contentt">
-                          <p>Tiên lượng</p>
-                          <h1>Đái tháo đường</h1>
-                        </div>
-                      </div>
+                    <div className="contentt">
+                      <p>Tiên lượng</p>
+                      <h1>Đái tháo đường</h1>
                     </div>
+                  </div>
+                </div>
 
-                    {/* <div className="language">
+                {/* <div className="language">
                       <div className="button-">
                         <div className="button-base">
                           <div className="text-v">VN</div>
@@ -114,31 +64,13 @@ class Header extends React.Component {
                         <div className="call-text">Liên hệ</div>
                       </div>
                     </div> */}
-                  </div>
-                </div>
               </div>
-            </Toolbar>
-          </AppBar>
-        </HideOnScroll>
+            </div>
+          </div>
+        </AppBar>
       </React.Fragment>
     );
   }
 }
 
-const mapStateToProps = (state) => {
-  return {
-    stateStatus: state.status,
-  };
-};
-
-const mapDispatchToProps = (dispatch) => {
-  let _action = new Action();
-
-  return {
-    resetOpen: () => {
-      dispatch(_action.resetOpen());
-    },
-  };
-};
-
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Header));
+export default Header;
